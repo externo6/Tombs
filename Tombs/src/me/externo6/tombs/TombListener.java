89b8b850822cdba49ca60c1ae68b4bb64f93b7f5
externo6 <joebutler_12@hotmail.com>
2014-01-19 16:49:02 +0000

    Release

---
+++ Tombs/src/me/externo6/tombs/TombListener.java
@@ -0,0 +1,34 @@
+package me.externo6.tombs;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class TombListener implements Listener{
+	public static Tombs plugin;
+	
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event){
+		Player player = event.getPlayer();
+		if(player.getItemInHand() != null && player.getItemInHand().hasItemMeta())
+		if (event.getPlayer().getItemInHand().getType().equals(Material.QUARTZ)){
+		if(player.getItemInHand().getItemMeta().hasLore());
+		if(player.getItemInHand().getItemMeta().getDisplayName().contentEquals("Tomb Artifact"));			
+		player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 150, 1));
+		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 500, 1));
+		player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 500, 1));
+		player.sendMessage(ChatColor.GOLD + "The Artifact is surging with energy!");
+
+			}
+		
+		
+}
+}
+
+
+
---
+++ Tombs/src/me/externo6/tombs/Tombs.java
@@ -0,0 +1,30 @@
+package me.externo6.tombs;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public class Tombs extends JavaPlugin {	
+	public final Logger logger = Logger.getLogger("minecraft");
+	public static Tombs plugin;
+	public final TombListener pl = new TombListener();
+
+	@Override
+	public void onDisable(){
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has Been Disabled!"); 
+	}
+
+	@Override
+	public void onEnable(){
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info("[Tombs]" + " Version " + pdfFile.getVersion() + " |" + " Developed for Banxsi.com by externo6");
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this.pl, this);
+		
+	}
+
+}
+
