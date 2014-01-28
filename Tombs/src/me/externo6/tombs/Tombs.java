8468989c1739895efa635822822c10a69918b1b4
externo6 <joebutler_12@hotmail.com>
2014-01-28 23:31:19 +0000

    Removed Listener class. Added cooldowns, and delays.

--- Tombs/src/me/externo6/tombs/Tombs.java
+++ Tombs/src/me/externo6/tombs/Tombs.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.util.List;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
@@ -17,21 +19,25 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
  
 public class Tombs extends JavaPlugin implements Listener {	
 	public final Logger logger = Logger.getLogger("minecraft");
 	public static Tombs plugin;
-	public final TombListener pl = new TombListener();
 	static File respawnFile = new File("./plugins/Tombs", "Respawns.yml");
     public static FileConfiguration respawns = YamlConfiguration.loadConfiguration(respawnFile);
 	static File checkpointFile = new File("./plugins/Tombs", "Checkpoint.yml");
 	public static FileConfiguration checkpoint = YamlConfiguration.loadConfiguration(checkpointFile);
-	  
+
+	//**Should Really be in another thread?
 	  @EventHandler
 	  public void onPlayerRespawn(PlayerRespawnEvent event)
 	  {
@@ -47,8 +53,6 @@ public class Tombs extends JavaPlugin implements Listener {
 	  }
 	  }
 	  
-
-	  
 	  public String LocToString(Location location)
 	  {
 	    int x = (int)location.getX();
@@ -59,10 +63,10 @@ public class Tombs extends JavaPlugin implements Listener {
 	  }
 	  
 	  @EventHandler
-	  public void onPlayerInteract(PlayerInteractEvent event)
+	  public void onPlayerInteract1(PlayerInteractEvent event)
 	  {
 	    Player player = event.getPlayer();
-	    if ((player.hasPermission("Checkpoint.use")) && 
+	    if ((player.hasPermission("tombs.use")) && 
 	      (event.getClickedBlock() != null))
 	    {
 	      Block b = event.getClickedBlock();
@@ -108,10 +112,9 @@ public class Tombs extends JavaPlugin implements Listener {
 	public void onEnable(){
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info("[Tombs]" + " Version " + pdfFile.getVersion() + " |" + " Developed for Banxsi.com by externo6");
+		@SuppressWarnings("unused")
 		PluginManager pm = getServer().getPluginManager();
-		getServer().getPluginManager().registerEvents(this, this);
-		pm.registerEvents(this.pl, this);
-		
+		getServer().getPluginManager().registerEvents(this, this);	
 	}
 	
 
@@ -136,7 +139,7 @@ public class Tombs extends JavaPlugin implements Listener {
 	        player.sendMessage(ChatColor.DARK_AQUA + " ---------- " + ChatColor.GOLD + "Tombs " + ChatColor.AQUA + getDescription().getVersion() + ChatColor.GOLD + " by " + ChatColor.AQUA + "externo6 " + ChatColor.DARK_AQUA + "----------");
 	        player.sendMessage(ChatColor.GOLD + "Add: " + ChatColor.GRAY + "Add a checkpoint to your target block.");
 	        player.sendMessage(ChatColor.GOLD + "Remove: " + ChatColor.GRAY + "Remove checkpoint from your target block.");
-	        player.sendMessage(ChatColor.GOLD + "Cancel: " + ChatColor.GRAY + "Removes the Checkpoint");
+	        player.sendMessage(ChatColor.GOLD + "Cancel: " + ChatColor.GRAY + "Removes your Checkpoint");
 	        player.sendMessage(ChatColor.GOLD + "Checkpoint: " + ChatColor.GRAY + "Goto last Checkpoint.");
 	        player.sendMessage(ChatColor.DARK_AQUA + " -----------------------------------------");
 	      }
@@ -147,7 +150,8 @@ public class Tombs extends JavaPlugin implements Listener {
 	        {
 	          if (player.hasPermission("tombs.add"))
 	          {
-	            Block b = player.getTargetBlock(null, 10);
+	            @SuppressWarnings("deprecation")
+				Block b = player.getTargetBlock(null, 10);
 	            String loc = LocToString(b.getLocation());
 	            if ((b.getType() == Material.STONE_PLATE) || (b.getType() == Material.WOOD_PLATE) || (b.getType() == Material.WOOD_BUTTON) || (b.getType() == Material.IRON_PLATE) || (b.getType() == Material.GOLD_PLATE) || (b.getType() == Material.LEVER) || (b.getType() == Material.STONE_BUTTON))
 	            {
@@ -174,7 +178,8 @@ public class Tombs extends JavaPlugin implements Listener {
 	        {
 	          if (player.hasPermission("Checkpoint.remove"))
 	          {
-	            Block b = player.getTargetBlock(null, 10);
+	            @SuppressWarnings("deprecation")
+				Block b = player.getTargetBlock(null, 10);
 	            String loc = LocToString(b.getLocation());
 	            if ((b.getType() == Material.STONE_PLATE) || (b.getType() == Material.WOOD_PLATE) || (b.getType() == Material.WOOD_BUTTON) || (b.getType() == Material.IRON_PLATE) || (b.getType() == Material.GOLD_PLATE) || (b.getType() == Material.LEVER) || (b.getType() == Material.STONE_BUTTON))
 	            {
@@ -265,7 +270,95 @@ public class Tombs extends JavaPlugin implements Listener {
 	      }
 	    }
 	    return false;
+	    //**Artifact Detection
 	  }
+	  @EventHandler
+		public void onPlayerInteract(PlayerInteractEvent event){ //If they are not in the correct world, they will not be teleported.
+			Player player = event.getPlayer();
+			if(event.getAction()==Action.RIGHT_CLICK_AIR){
+			if (Cooldowns.tryCooldown(player, "1", 5000)) {
+			if(player.getWorld().getName().equalsIgnoreCase("1point7")){
+			if(player.getItemInHand() != null && player.getItemInHand().hasItemMeta()) //Check if ItemMeta is present, this stops nullpointerexeptions on normal Quartz
+			if (event.getPlayer().getItemInHand().getType().equals(Material.QUARTZ)){
+			if(player.getItemInHand().getItemMeta().hasLore());
+			if(player.getItemInHand().getItemMeta().getDisplayName().contentEquals("Tomb Artifact"));			
+			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 150, 1));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 500, 1));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 500, 1));
+			player.sendMessage(ChatColor.GOLD + "The Artifact is surging with energy!");
+			player.sendMessage(ChatColor.AQUA + "You feel an energy build up, then fade away...");
+			player.setVelocity(new Vector(0,1,0));
+			}
+			}else{ //if they are in the correct world, they will be teleported to the start of Tomb2
+				if(player.hasPermission("tombs.canwarpto")){
+				if(player.getWorld().getName().equalsIgnoreCase("dun1")){
+				if(player.getItemInHand() != null && player.getItemInHand().hasItemMeta()) //Check if ItemMeta is present, this stops nullpointerexeptions on normal Quartz
+				if (event.getPlayer().getItemInHand().getType().equals(Material.QUARTZ)){
+				if(player.getItemInHand().getItemMeta().hasLore());
+				if(player.getItemInHand().getItemMeta().getDisplayName().contentEquals("Tomb Artifact"));			
+				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 180, 1));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 500, 1));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 500, 1));
+				player.sendMessage(ChatColor.GOLD + "The Artifact is surging with energy");
+				player.sendMessage(ChatColor.GREEN + "You feel an energy build up and flung into the air!");
+				jump(player);
+				blindness(player);
+				delay(player);
+				ouch(player);
+				}
+				}
+				}else{
+					if(player.getItemInHand() != null && player.getItemInHand().hasItemMeta()) //Check if ItemMeta is present, this stops nullpointerexeptions on normal Quartz
+						if (event.getPlayer().getItemInHand().getType().equals(Material.QUARTZ)){
+						if(player.getItemInHand().getItemMeta().hasLore());
+						if(player.getItemInHand().getItemMeta().getDisplayName().contentEquals("Tomb Artifact"));			
+						player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 150, 1));
+						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 500, 1));
+						player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 500, 1));
+						player.sendMessage(ChatColor.GOLD + "The Artifact is surging with energy!");
+						player.sendMessage(ChatColor.RED + "You Dont have Permission to go there yet...");
+						
+	}
+	}
+	}
+	}
+	}
+	}
+		public void delay (final Player player){	
+			getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				  public void run() {
+			player.teleport(new Location(player.getWorld(), -395.5, 106, 1424.5, 180 , 0));
+				  }
+			}, 80L);
+			}
+		
+		public void jump (final Player player){
+			getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				  public void run() {
+						Location loc = player.getLocation();
+				  player.setVelocity(new Vector(0,3,0));
+					player.playSound(loc, Sound.ENDERDRAGON_WINGS, 1, 1);
+				  }
+			}, 40L);
+			}
+		
+		public void blindness (final Player player){
+			getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				  public void run() {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 55, 1));	
+				  }
+			}, 50L);
+			}
+		public void ouch (final Player player){
+			getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				  public void run() {
+					  Location loc = player.getLocation();
+			player.sendMessage(ChatColor.AQUA + "Ouch!");
+			loc.getWorld().playEffect(loc, Effect.MOBSPAWNER_FLAMES, 2004);
+			player.playSound(loc, Sound.FALL_BIG, 1, 1);
+				  }
+			}, 110L);
+			}
 	}
 
 
